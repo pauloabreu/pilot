632fc1857d4af314cc109c4eb88a7644e7116c0b
Vagner S <vagnervst17@gmail.com>
2019-01-11 16:35:10 -0200

    transaction: add token capture disclaimer link

--- packages/pilot/src/containers/Capture/Form/index.js
+++ packages/pilot/src/containers/Capture/Form/index.js
@@ -117,7 +117,7 @@ const CaptureForm = ({
                 <span className={style.captureWarning}>
                   {t('pages.capture.token_capture_warning')}
                   {' '}
-                  <a href="https://google.com">
+                  <a href="https://gist.github.com/vagnervst/781adafc855fd9a51a94417731d9fe2f">
                     {t('pages.capture.token_capture_link')}
                   </a>
                 </span>
